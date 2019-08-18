d3849641d75cd2cf4ef57cc0978f6d9412424cab
Pumper Knickle <pumperknickle@protonmail.com>
2019-08-18 14:35:24 -0700

    upgrade cryptostarterpack

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Regenerate"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pumperknickle/CryptoStarterPack.git", from: "1.0.0"),
+        .package(url: "https://github.com/pumperknickle/CryptoStarterPack.git", from: "1.0.2"),
         .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
     ],
