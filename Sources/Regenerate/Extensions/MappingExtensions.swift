a7188a806ace63b736feacb4c6dd562339fb82c3
CAHjbao <pumperknickle@protonmail.com>
2019-10-26 12:09:19 -0500

    refactor

--- Sources/Regenerate/Extensions/MappingExtensions.swift
+++ Sources/Regenerate/Extensions/MappingExtensions.swift
@@ -2,12 +2,6 @@ import Foundation
 import AwesomeDictionary
 
 public extension Mapping where Value == [[String]] {
-    func prepend(_ key: String) -> Mapping<Key, Value> {
-        return elements().lazy.reduce(Mapping<Key, Value>()) { (result, entry) -> Mapping<Key, Value> in
-            result.setting(key: entry.0, value: entry.1.map { [key] + $0 })
-        }
-    }
-    
     static func + (lhs: Mapping<Key, Value>, rhs: Mapping<Key, Value>) -> Mapping<Key, Value> {
         return rhs.elements().lazy.reduce(lhs) { (result, entry) -> Mapping<Key, Value> in
             guard let current = result[entry.0] else { return result.setting(key: entry.0, value: entry.1) }
--- Sources/Regenerate/Implementations/RGDictionary256.swift
+++ Sources/Regenerate/Implementations/RGDictionary256.swift
@@ -35,32 +35,3 @@ extension RGDictionary256: RGDictionary {
 	public var masks: TrieSet<Edge>! { return rawMasks ?? TrieSet<Edge>() }
 	public var isMasked: Bool! { return rawIsMasked != nil ? true : false }
 }
-
-//import Foundation
-//import Bedrock
-//
-//public struct RGDictionary256<Key: Stringable, Value: CID>: Codable where Value.Digest == UInt256 {
-//    private let rawCore: RGRT256<Key, Value>!
-//    private let rawMapping: [Key: Value]!
-//    private let rawIncompleteChildren: Set<Key>!
-//}
-//
-//extension RGDictionary256: RGArtifact {
-//    public typealias Digest = UInt256
-//}
-//
-//extension RGDictionary256: RGDictionary {
-//    public typealias Key = Key
-//    public typealias Value = Value
-//    public typealias CoreType = RGRT256<Key, Value>
-//    
-//    public var core: RGRT256<Key, Value>! { return rawCore }
-//    public var mapping: [Key : Value]! { return rawMapping }
-//    public var incompleteChildren: Set<Key>! { return rawIncompleteChildren }
-//    
-//    public init(core: RGRT256<Key, Value>, mapping: [Key : Value], incomplete: Set<Key>) {
-//        self.rawCore = core
-//        self.rawMapping = mapping
-//        self.rawIncompleteChildren = incomplete
-//    }
-//}
